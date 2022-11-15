82c534229317a5b7afebcd3e3655f495bc34e2c0
James <jamesmdolezal@gmail.com>
2022-11-15 13:34:43 -0600

    Cleaner legacy patch fix for loading old network pickles

--- stylegan3/torch_utils/legacy_fix.py
+++ stylegan3/torch_utils/legacy_fix.py
@@ -1,22 +1,12 @@
 def apply_legacy_patch(src):
-    src = src.replace(
-        '''from ..torch_utils import misc, persistence
-from ..torch_utils.ops import bias_act, conv2d_resample, fma, upfirdn2d''',
-        '''try:
-    from ..torch_utils import misc, persistence
-    from ..torch_utils.ops import bias_act, conv2d_resample, fma, upfirdn2d
-except ImportError:
-    from torch_utils import misc, persistence
-    from torch_utils.ops import bias_act, conv2d_resample, fma, upfirdn2d'''
-    )
-    src = src.replace(
-        '''from ..torch_utils import misc, persistence
-from ..torch_utils.ops import conv2d_gradfix, grid_sample_gradfix, upfirdn2d''',
-        '''try:
-    from ..torch_utils import misc, persistence
-    from ..torch_utils.ops import conv2d_gradfix, grid_sample_gradfix, upfirdn2d
-except ImportError:
-    from torch_utils import misc, persistence
-    from torch_utils.ops import conv2d_gradfix, grid_sample_gradfix, upfirdn2d'''
-    )
-    return src
+
+    def wrap_import(line):
+        if line.startswith('from ..torch_utils'):
+            return ('try:'
+                    f'\n\t{line}'
+                    '\nexcept:'
+                    f'\n\t{line.replace("..torch_utils", "torch_utils")}')
+        else:
+            return line
+
+    return '\n'.join([wrap_import(line) for line in src.split('\n')])
