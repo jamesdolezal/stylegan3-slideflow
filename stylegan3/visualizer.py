f16bfe8841819fe6d16f2aef0e6c8e91dcdb093c
James <jamesmdolezal@gmail.com>
2023-04-03 07:54:45 -0500

    Slideflow compatibility update (1.5.x)

--- stylegan3/visualizer.py
+++ stylegan3/visualizer.py
@@ -24,7 +24,7 @@ from .gui_utils import imgui_utils
 from .gui_utils import gl_utils
 from .gui_utils import text_utils
 
-from slideflow.studio.widgets import ModelWidget
+from slideflow.workbench.model_widget import ModelWidget
 
 try:
     from . import dnnlib
